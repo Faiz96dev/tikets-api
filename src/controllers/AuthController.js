3a4f6c00b8455734566065a47137b6cdfce585ca
Faiz96dev <faiz96dev@gmail.com>
2020-04-20 18:54:18 +0600

    test1

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,20 +1,27 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const logger = require('../services/LoggerService');
 const AuthService = require('../services/AuthService');
-const {errors} = require('../constants/messages');
+const { errors } = require('../constants/messages');
 
 const AuthRouter = Router();
 
-AuthRouter.get('/reg', async (req, res) => {
-    try {
-        const {email} = req.body
-        const {password} = req.body
-        const auth = await AuthService.CreateUser(email, password);
-        return res.status(200).json(auth);
-    } catch (err) {
-        logger.error(err.message);
-        return res.status(500).json(errors.common[500]);
+AuthRouter.post('/reg', async (req, res) => {
+  try {
+    console.log('pow');
+    const { email } = req.body;
+    const { password } = req.body;
+    if (password || email) {
+      return res.status(500)
+        .json('token or password is not provided !');
     }
+    const auth = await AuthService.CreateUser(email, password);
+    return res.status(200)
+      .json(auth);
+  } catch (err) {
+    logger.error(err.message);
+    return res.status(500)
+      .json(errors.common[500]);
+  }
 });
 
 module.exports = AuthRouter;
--- src/repositories/UsersRepository.js
+++ src/repositories/UsersRepository.js
@@ -4,7 +4,13 @@ const getAllUsers = () => UserEntity.find();
 
 const FindUser = (email) => UserEntity.find(email);
 
-const AddUser = (email) => UserEntity.find(email);
+const AddUser = (email, hash) => {
+    var user = new UserEntity({passwordHash: hash, email});
+    user.save((err, user) => {
+        if (err) return console.error(err);
+        console.log(user.email + " user saved to  collection.");
+    });
+};
 
 module.exports = {
     getAllUsers,
